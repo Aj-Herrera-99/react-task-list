c2860ad2304a1f6b9f89f0524d97fda377d1f611
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-11 13:49:15 +0100

    macrostruttura

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-    <>
-      <h1>Hello from App component</h1>
-    </>
-  )
+    return (
+        <>
+            {/* HEADER */}
+            <header className="bg-red-300">
+                <h1>Task Manager</h1>
+            </header>
+            {/* CURRENT TASKS */}
+            <section className="bg-green-300">
+                <h2>Current Tasks</h2>
+            </section>
+            {/* COMPLETED TASKS */}
+            <section className="bg-blue-300">
+                <h2>Completed Tasks Tasks</h2>
+            </section>
+        </>
+    );
 }
 
-export default App
+export default App;
