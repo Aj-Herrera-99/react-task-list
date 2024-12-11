6c698ee87a974dc8eeed51839d6ac05b35b57a36
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-11 14:03:12 +0100

    jsx logic (testing)

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,27 @@ function App() {
     return (
         <>
             {/* HEADER */}
-            <header className="bg-red-300">
+            <header className="bg-red-300 p-4">
                 <h1>Task Manager</h1>
             </header>
             {/* CURRENT TASKS */}
-            <section className="bg-green-300">
+            <section className="bg-green-300 p-4">
                 <h2>Current Tasks</h2>
+                <ul>
+                    {tasks.map((task) => (
+                        <li className="flex flex-col">
+                            <div className="flex gap-3">
+                                <h3>{task.title}</h3>
+                                <span>{task.state}</span>
+                            </div>
+                            <span> Priority: {task.priority}</span>
+                            <span>Est. time {task.estimatedTime}</span>
+                        </li>
+                    ))}
+                </ul>
             </section>
             {/* COMPLETED TASKS */}
-            <section className="bg-blue-300">
+            <section className="bg-blue-300 p-4">
                 <h2>Completed Tasks Tasks</h2>
             </section>
         </>
