95c40402a0caf436428e6dab8c3a4f7d2d58147b
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-12-11 14:31:09 +0100

    risolto errore della key univoca -> aggiunto ad ogni li una prop key uguale all'id del task che lo contiene

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import "./App.css";
 import { tasks } from "./data/tasks";
 
 function App() {
+    // filtro tasks in corso
     const currentTasks = tasks.filter(
         (task) => task.state.toLowerCase() != "completed"
     );
+    // numero tasks in corso
     const currTasksLen = currentTasks.length;
 
+    // filtro tasks completate
     const completedTasks = tasks.filter(
         (task) => task.state.toLowerCase() == "completed"
     );
+    // numero tasks completate
     const completedTasksLen = completedTasks.length;
 
     return (
@@ -18,6 +22,7 @@ function App() {
             <header className="p-6 bg-green-300">
                 <h1 className="text-4xl font-semibold">Task Manager</h1>
             </header>
+
             {/* CURRENT TASKS */}
             <section className="p-6">
                 <h2 className="text-xl font-semibold">
@@ -25,7 +30,7 @@ function App() {
                 </h2>
                 <ul className="my-4">
                     {currentTasks.map((task) => (
-                        <li className="flex flex-col my-3">
+                        <li key={task.id} className="flex flex-col my-3">
                             <div className="flex gap-3 font-semibold">
                                 <h3>{task.title}</h3>
                                 <span>{task.state}</span>
@@ -36,10 +41,12 @@ function App() {
                     ))}
                 </ul>
             </section>
+
             {/* SEPARATOR (ONLY GRAPHICS) */}
             <div className="px-6 ">
                 <div className="border-solid border-b-2 border-stone-300"></div>
             </div>
+
             {/* COMPLETED TASKS */}
             <section className="p-6">
                 <h2 className="text-xl font-semibold">
@@ -47,7 +54,7 @@ function App() {
                 </h2>
                 <ul className="my-4">
                     {completedTasks.map((task) => (
-                        <li className="flex flex-col my-3">
+                        <li key={task.id} className="flex flex-col my-3">
                             <div className="flex gap-3 font-semibold">
                                 <h3>{task.title}</h3>
                                 <span>{task.state}</span>
